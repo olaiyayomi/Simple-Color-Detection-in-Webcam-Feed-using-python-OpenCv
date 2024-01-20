e62b79aa8be44b6fc108284ad48b1ff8a3e2e2d0
Olaiya Yomi Williams <olaiyayomi37@gmail.com>
2024-01-20 17:29:10 +0100

    Create color_detection.py

---
+++ color_detection.py
@@ -0,0 +1,33 @@
+import cv2 as cv
+import numpy as np
+import sys
+
+video = cv.VideoCapture(0)
+
+if not video.isOpened():
+    sys.exit("no camera")
+
+while True:
+    ret, frame = video.read()
+    
+    if not ret:
+        print("not all frame captured")
+    
+    cvt = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+    
+    lower_blue = np.array([110, 50, 50])
+    high_blue = np.array([130, 255, 255])
+    
+    new = cv.inRange(cvt, lower_blue, high_blue)
+    
+    kervel = np.ones((5,5), np.uint8)
+    reduce = cv.morphologyEx(new, cv.MORPH_OPEN, kervel)
+    
+    cv.imshow("video", reduce)
+    
+    key = cv.waitKey(1)
+    
+    if key == ord("q"):
+        break
+cv.destroyAllWindows()
+video.release()
